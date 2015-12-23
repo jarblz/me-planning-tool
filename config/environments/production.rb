8f0d517431f9a99d3caf388228a6cc5ee8f89c6d
Jared Ables <jaredables@gmail.com>
2015-12-22 22:13:34 -0500

    heroku tweaks

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -63,7 +63,7 @@ Rails.application.configure do
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
-  
+
   # Default URL options for the mailer
   config.action_mailer.default_url_options = { host: ENV['host'], port: ENV['PORT'] }
 
